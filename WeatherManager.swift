9832780ea3f9d64aad10fd021309a8f3a3cdb184
Chris Williams <chriswilliams14.williams@gmail.com>
2020-02-11 17:20:56 +0000

    JSON decoder created and weather model created

--- WeatherManager.swift
+++ WeatherManager.swift
@@ -13,7 +13,7 @@ struct WeatherManager {
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-       performRequest(urlString: urlString)
+        performRequest(urlString: urlString)
     }
     
     func performRequest(urlString: String) {
@@ -24,22 +24,34 @@ struct WeatherManager {
             let session = URLSession(configuration: .default)
             
             // 3. Give the session a task
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
-             
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
+            
             // 4. Start the task
             task.resume()
         }
     }
     
-    func handle(data: Data?, response: URLResponse?, error: Error?) {
-        if error != nil {
-            print(error!)
-            return
-        }
-        
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString!)
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionID: id, cityName: name, temperature: temp)
+            
+            print(weather.temperatureString)
+        } catch {
+            print(error)
         }
     }
 }
